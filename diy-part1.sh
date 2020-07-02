2ad4cda43ad30d5eabdf811cbd361adc21849794
blueuld <43904046+blueuld@users.noreply.github.com>
2020-07-03 01:31:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,3 @@
 #mkdir package/base-files/files/config
 #echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
 echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
-./scripts/feeds update -a && ./scripts/feeds install -a
